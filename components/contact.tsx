e2ed6a0917e5aeb90b01287dd6e7ccc8d45c7ca5
muazalmuttaki <muazalmuttaki@gmail.com>
2026-01-26 01:02:32 +0600

    update

--- components/contact.tsx
+++ components/contact.tsx
@@ -61,7 +61,7 @@ export default function Contact() {
         >
           {/* Left Info */}
           <div className="p-8 sm:p-10">
-            <h3 className="text-3xl md:text-4xl lg:text-5xl font-semibold text-gray-900 root">
+            <h3 className="text-2xl md:text-4xl lg:text-5xl font-semibold text-gray-900 root">
               Contact Information
             </h3>
             <p className="mt-3 text-gray-600">
